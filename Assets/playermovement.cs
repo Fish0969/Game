7383ef8968dbe8936440036cc631c7b236071c7a
Fish0969 <matefisher@gmail.com>
2025-10-23 16:51:49 +0200

    Camera update

--- Assets/playermovement.cs
+++ Assets/playermovement.cs
@@ -2,25 +2,44 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
-    Rigidbody rb;
-    [SerializeField] float MoveSpeed = 10;
-    [SerializeField] float Jump = 5;
+    [SerializeField]
+    private float speed = 5f;
 
+    [SerializeField]
+    private float mouseSensitivity = 5f;
+
+    private Vector3 moveDirection;
+    private float rotationY;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horInput = Input.GetAxis("Horizontal") * MoveSpeed;
-        float verInput = Input.GetAxis("Vertical") * MoveSpeed;
-
-        rb.linearVelocity = new Vector3(horInput, rb.linearVelocity.y, verInput);
-        if (Input.GetButtonDown("Jump")  && Mathf.Approximately(rb.linearVelocity.y,0)) rb.linearVelocity = new Vector3(rb.linearVelocity.x, Jump, rb.linearVelocity.z);
-
+        HandleMovement();
+        HandleRotation();
     }
+    private void HandleMovement()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(Horizontal, 0f, Vertical).normalized;
+
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+    }
+    private void HandleRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        rotationY += mouseX * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(0f, rotationY, 0f);
+        
+    }
+
+
 }
