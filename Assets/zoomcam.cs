ae8ad0247fd97b9cafa4a61844c4415cd4656b05
Fish0969 <matefisher@gmail.com>
2025-10-24 00:01:18 +0200

    Updated zooming system

--- Assets/zoomcam.cs
+++ Assets/zoomcam.cs
@@ -12,6 +12,7 @@ public class zoomcam : MonoBehaviour
     private Vector3 originalPosition;
     private Quaternion originalRotation;
     private bool isZoomed = false;
+    private float Xrotation;
     
     void Start()
     {
@@ -26,27 +27,31 @@ public class zoomcam : MonoBehaviour
         originalPosition = targetCapsule.position - targetCapsule.forward * 5f + Vector3.up * 2f;
         originalRotation = Quaternion.LookRotation(targetCapsule.position - originalPosition);
         
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Zoom"))
         {
             isZoomed = !isZoomed; 
         }
 
         if (isZoomed && targetCapsule != null)
         {
-           
-            Vector3 targetPosition = targetCapsule.position - targetCapsule.forward * -3f + Vector3.up * -.5f;
-            
+
+            Vector3 targetPosition = targetCapsule.position - targetCapsule.forward * 0f + Vector3.up * 0f;
+
+            float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * 500;
+            Xrotation -= mouseY;
+            Xrotation = Mathf.Clamp(Xrotation, -90f, 90f);
+            transform.rotation = Quaternion.Euler(Xrotation, transform.rotation.eulerAngles.y, 0);
 
             transform.position = Vector3.Lerp(transform.position, targetPosition, zoomSpeed * Time.deltaTime);
             
             Quaternion targetRotation = Quaternion.LookRotation(targetCapsule.position - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, zoomSpeed * Time.deltaTime);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, zoomSpeed * Time.deltaTime);
         }
         else
         {
             transform.position = Vector3.Lerp(transform.position, originalPosition, zoomSpeed * Time.deltaTime);
             transform.rotation = Quaternion.Slerp(transform.rotation, originalRotation, zoomSpeed * Time.deltaTime);
         }
-
+        
     }
 }
