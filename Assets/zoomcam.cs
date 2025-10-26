1f03782491340e3b141a720dcf8d63143c3a6a3b
Fish0969 <matefisher@gmail.com>
2025-10-26 22:06:58 +0100

    Book
    Added a book

--- Assets/zoomcam.cs
+++ Assets/zoomcam.cs
@@ -35,7 +35,8 @@ public class zoomcam : MonoBehaviour
         {
             isZoomed = !isZoomed;
             crosshair.SetActive(isZoomed);
-            hair.SetActive(!isZoomed);            
+            hair.SetActive(!isZoomed);
+            
 
         }
 
