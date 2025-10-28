7f4d2fa5ad92a0c70e40f6adceb8a456953353d4
Fish0969 <matefisher@gmail.com>
2025-10-28 18:34:09 +0100

    Enemies random spawn
    now enemies spawn randomly

---
+++ Assets/GeneratingEnemies.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+public class GeneratingEnemies : MonoBehaviour
+{
+    public GameObject enemy;
+    public int Xpos;
+    public int Zpos;
+    public int enemyCount;
+
+    void Start()
+    {
+        StartCoroutine(Enemydrop());
+    }
+
+    IEnumerator Enemydrop()
+    {
+        while (enemyCount < 10)
+        {
+            Xpos = Random.Range(-20, 10);
+            Zpos = Random.Range(3, 30);
+            Instantiate(enemy, new Vector3(Xpos, 0, Zpos), Quaternion.identity);
+            yield return new WaitForSeconds(0.2f);
+            enemyCount+=1;
+        
+        
+        }
+    }
+
+}
+
