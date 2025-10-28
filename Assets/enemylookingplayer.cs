9bc97475b0f250945f18a71295b069f6bdc28641
Fish0969 <matefisher@gmail.com>
2025-10-28 19:01:32 +0100

    Enemy update
    enemies follow you

--- Assets/enemylookingplayer.cs
+++ Assets/enemylookingplayer.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 public class enemylookingplayer : MonoBehaviour
 {
     public Transform player;
+    private Vector3 movement;
+    public float speed = 1f;
+    public Rigidbody rb;
 
     void Update()
     {
+
         this.gameObject.transform.LookAt(player);
+
+        movement = transform.forward * speed;
+        rb.linearVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z);
     }
 }
